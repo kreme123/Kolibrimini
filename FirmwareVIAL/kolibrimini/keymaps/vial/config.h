da0643cdbe6ebfab3159933645f1cf852660bf1f
kreme123 <85822364+kreme123@users.noreply.github.com>
2022-11-10 23:47:44 +0200

    Add files via upload

---
+++ FirmwareVIAL/kolibrimini/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD5, 0xFF, 0x49, 0x8E, 0x2E, 0x19, 0xBF, 0x7E}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {1, 6}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
